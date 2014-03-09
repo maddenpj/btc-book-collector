91748c00c3aec55a7157bc3d27a81b703573b5d3
Patrick Madden <maddenpj@gmail.com>
2014-03-09 12:33:17 -0500

    Modified pusher client, added package json, consolidated feeds

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
-var PusherClient = require('pusher-node-client').PusherClient
+var PusherClient = require('./pusher-node-client').PusherClient
 
 var client = new PusherClient({key: 'de504dc5763aeef9ff52', secret: ''});
 
-var channel = null;
+var book = null;
+var trades = null;
 client.on('connect', function () {
   console.log('connected');
 
-  channel = client.subscribe('order_book')
+  book = client.subscribe('order_book')
 
-  channel.on('success', function () {
-    channel.on('data', function (data) {
-      console.log(data);
+  book.on('success', function () {
+    book.on('data', function (data, ts) {
+      // console.log(data);
+      console.log('Order book change')
+      console.log(ts);
+    });
+  });
+
+
+  trades = client.subscribe('live_trades');
+
+  trades.on('success', function () {
+    trades.on('trade', function (data, ts) {
+      // console.log(data);
+      console.log('Trade');
+      console.log(ts);
     });
   });
 });
